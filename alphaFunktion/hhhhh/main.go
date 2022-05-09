3f403b0da1ae997365827814c972c49a625432ae
Thorsten Klöhn <thorstenkloehn@gmail.com>
2022-05-09 18:22:58 +0200

    Start

--- alphaFunktion/hhhhh/main.go
+++ alphaFunktion/hhhhh/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"crypto/rand"
+	"fmt"
+	"log"
+)
+
 func main() {
 
+	data, err := generateRandomBytes(16)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(data)
+}
+
+func generateRandomBytes(n int) ([]byte, error) {
+
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
 }
