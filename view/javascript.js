dc54e14d6acd2950d0a416a3d9785ae6da75bf98
Thorsten Klöhn <thorstenkloehn@gmail.com>
2022-05-20 09:42:06 +0200

    Auenwanderweg

--- view/javascript.js
+++ view/javascript.js
@@ -12,7 +12,23 @@ L.tileLayer('https://ahrensburg.city/karte/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://ahrensburg.city">ahrensburg.city</a>,<a href="./docs/Impressum">Impressum</a>,<a href="./docs/Datenschutzerklärung">Datenschutzerklärung</a> , </a><a href="./docs/">Dokument</a>,&copy;<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+var gpx = './gpx/Auewanderweg/auewanderweg_gpx_20220519_191032.gpx'; // URL to your GPX file or the GPX itself
 
+new L.GPX(gpx, {
+    async: true,
+    marker_options: {
+        startIconUrl: 'static/Leaflet/gpx/pin-icon-start.png',
+        endIconUrl: 'static/Leaflet/gpx/pin-icon-end.png',
+        shadowUrl: 'static/Leaflet/gpx/pin-shadow.png',
+        wptIconUrls: 'static/Leaflet/gpx/pin-icon-wpt.png'
+    }
+}).on('loaded', function(e) {
+    map.fitBounds(e.target.getBounds());
+    var ergenis = e.target;
+    var info = "Auewanderweg"
+    ergenis.bindPopup(info);
+
+}).addTo(map);
 
 let laden = new XMLHttpRequest;
 laden.open('GET', 'geoserver/Ahrensburg/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Ahrensburg%3Aahrensburg&maxFeatures=50&outputFormat=application%2Fjson', true);
@@ -28,16 +44,3 @@ laden.onload = function () {
 
 laden.send();
 
-var gpx = './gpx/Auewanderweg/auewanderweg_gpx_20220519_191032.gpx'; // URL to your GPX file or the GPX itself
-
-new L.GPX(gpx, {
-    async: true,
-    marker_options: {
-        startIconUrl: 'static/Leaflet/gpx/pin-icon-start.png',
-        endIconUrl: 'static/Leaflet/gpx/pin-icon-end.png',
-        shadowUrl: 'static/Leaflet/gpx/pin-shadow.png',
-        wptIconUrls: 'static/Leaflet/gpx/pin-icon-wpt.png'
-    }
-}).on('loaded', function(e) {
-    map.fitBounds(e.target.getBounds());
-}).addTo(map);
